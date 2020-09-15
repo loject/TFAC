35edfac6328fc4dd5d778830a870332334f6abc6
loject <29097142+loject@users.noreply.github.com>
2020-09-15 23:22:00 +0700

    add simple runnerAttendace model

---
+++ AAC/Models/RunnerAttendances.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AAC.Models
+{
+    public class RunnerAttendancesModel
+    {
+        public string Name { get; set; }
+        public List<DateTime> Attendaces { get; set; }
+    }
+}
