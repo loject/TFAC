212c85e8fd490ab184221bb2adb65743e31dd936
loject <29097142+loject@users.noreply.github.com>
2020-09-18 16:02:20 +0700

    move to expander on view attanace history

--- AAC/Models/RunnerAttendances.cs
+++ AAC/Models/RunnerAttendances.cs
@@ -1,11 +1,18 @@
-﻿using System;
+﻿using PropertyChanged;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace AAC.Models
 {
+    [AddINotifyPropertyChangedInterface]
     public class RunnerAttendancesModel
     {
         public string Name { get; set; }
         public List<DateTime> Attendaces { get; set; }
+        public RunnerAttendancesModel()
+        {
+
+        }
     }
 }
