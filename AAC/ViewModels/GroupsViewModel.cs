33c6ac52eef8eb2c86568f099773e99cd4de1771
loject <29097142+loject@users.noreply.github.com>
2020-09-27 01:38:02 +0700

    add grouping

--- AAC/ViewModels/GroupsViewModel.cs
+++ AAC/ViewModels/GroupsViewModel.cs
@@ -1,5 +1,4 @@
 ﻿using PropertyChanged;
-using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Windows.Input;
@@ -7,17 +6,47 @@ using Xamarin.Forms;
 
 namespace AAC.ViewModels
 {
-    public class Group
+    public class Runner
     {
         public string Name { get; set; }
-        public ObservableCollection<string> RunnersNames { get; set; }
-        public Group()
+        public Runner()
         {
 
         }
     }
+    public class Group : ObservableCollection<Runner>
+    {
+        public string Name { get; set; }
+        public Group() : base()
+        {
+            AddNewRunner = new Command(AddRunner);
+        }
+        public Group(string name, ObservableCollection<Runner> runners) : base(runners)
+        {
+            Name = name;
+            AddNewRunner = new Command(AddRunner);
+        }
+
+        #region Commands
+        public ICommand AddNewRunner { get; private set; }
+        #endregion
+        #region Functions
+        private async void AddRunner()
+        {
+            var RunnerName = await App.Current.MainPage.Navigation.NavigationStack[^1].DisplayPromptAsync("Имя спортсмена", "");
+            if (string.IsNullOrWhiteSpace(RunnerName))
+            {
+                await App.Current.MainPage.Navigation.NavigationStack[^1].DisplayAlert("Ошибка", "Имя не может быть пустым", "Ок");
+            }
+            else
+            {
+                Items.Add(new Runner { Name = RunnerName });
+            }
+        }
+        #endregion
+    }
     [AddINotifyPropertyChangedInterface]
-    public class GroupsViewModel
+    public class GroupsViewModel 
     {
         public ObservableCollection<Group> Groups { get; set; }
         public GroupsViewModel()
@@ -32,7 +61,7 @@ namespace AAC.ViewModels
                 }
                 else
                 {
-                    Groups.Add(new Group { Name = GroupName, RunnersNames = new ObservableCollection<string>() });
+                    Groups.Add(new Group(GroupName, new ObservableCollection<Runner>() ));
                 }
             });
         }
