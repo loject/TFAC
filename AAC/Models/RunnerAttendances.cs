56249aec3e5332c0c2beee5881111c5825a02831
loject <29097142+loject@users.noreply.github.com>
2020-09-26 21:26:38 +0700

    fix little mistake

--- AAC/Models/RunnerAttendances.cs
+++ AAC/Models/RunnerAttendances.cs
@@ -1,7 +1,9 @@
 ﻿using PropertyChanged;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Linq;
 
 namespace AAC.Models
 {
@@ -12,61 +14,66 @@ namespace AAC.Models
         const double DaysPerYear = 356;
 
         public string Name { get; set; }
-        public List<DateTime> Attendaces { get; set; }
+        public ObservableCollection<DateTime> Attendaces { get; set; }
         public RunnerAttendancesModel()
         {
 
         }
         #region Properties
+        [DependsOn(nameof(Attendaces))]
         public DateTime FirstAttend
         {
             get
             {
-                Attendaces.Sort();
+                Attendaces.OrderBy(dt => dt);
                 return Attendaces[0];
             }
         }
+        [DependsOn(nameof(Attendaces))]
         public double AttenAvgMonth
         {
             get
             {
                 TimeSpan period = DateTime.Now - FirstAttend;
-                return period.TotalDays / DaysPerMonth / Attendaces.Count;
+                return Attendaces.Count / (period.TotalDays / DaysPerMonth);
             }
         }
+        [DependsOn(nameof(Attendaces))]
         public int AttenLastMonth
         {
             get
             {
-                Attendaces.Sort();
+                Attendaces.OrderBy(dt => dt);
                 int res = 0;
                 DateTime MonthAgo = DateTime.Now - TimeSpan.FromDays(DaysPerMonth);
-                for (int i = Attendaces.Count - 1; i >= 0; --i)
+                for (int i = 0; i < Attendaces.Count; ++i)
                 {
-                    if (Attendaces[i] < MonthAgo) res++;
+                    if (Attendaces[i] >= MonthAgo) res++;
                     else break;
                 }
                 return res;
             }
         }
+        [DependsOn(nameof(Attendaces))]
         public double AttenAvgYear
         {
             get
             {
                 TimeSpan period = DateTime.Now - FirstAttend;
-                return period.TotalDays / DaysPerYear / Attendaces.Count;
+                return Attendaces.Count / (period.TotalDays / DaysPerYear);
             }
         }
+        [DependsOn(nameof(Attendaces))]
         public int AttenLastYear
         {
             get
             {
-                Attendaces.Sort();
+                Attendaces.OrderBy(dt => dt);
                 int res = 0;
                 DateTime YearAgo = DateTime.Now - TimeSpan.FromDays(DaysPerYear);
-                for (int i = Attendaces.Count - 1; i >= 0; --i)
-                {
-                    if (Attendaces[i] < YearAgo) res++;
+                for (int i = 0; i < Attendaces.Count; ++i)
+                    {
+                    if (Attendaces[i] >= YearAgo) res++;
                     else break;
                 }
                 return res;
