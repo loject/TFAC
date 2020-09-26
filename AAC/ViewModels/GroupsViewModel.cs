51d9b1b1b780b0d2e29d18593af1230cf25f3936
loject <29097142+loject@users.noreply.github.com>
2020-09-27 00:13:37 +0700

    add create group button

--- AAC/ViewModels/GroupsViewModel.cs
+++ AAC/ViewModels/GroupsViewModel.cs
@@ -1,15 +1,42 @@
 ﻿using PropertyChanged;
+using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace AAC.ViewModels
 {
-    [AddINotifyPropertyChangedInterface]
-    public class GroupsViewModel
+    public class Group
     {
+        public string Name { get; set; }
         public ObservableCollection<string> RunnersNames { get; set; }
-        public GroupsViewModel()
+        public Group()
         {
 
         }
+    }
+    [AddINotifyPropertyChangedInterface]
+    public class GroupsViewModel
+    {
+        public ObservableCollection<Group> Groups { get; set; }
+        public GroupsViewModel()
+        {
+            Groups = new ObservableCollection<Group>();
+            CreateNewGroup = new Command(async () =>
+            {
+                var GroupName = await App.Current.MainPage.Navigation.NavigationStack[^1].DisplayPromptAsync("Имя группы", "");
+                if (string.IsNullOrWhiteSpace(GroupName))
+                {
+                    await App.Current.MainPage.Navigation.NavigationStack[^1].DisplayAlert("Ошибка", "Имя группы не может быть пустым", "Ок");
+                }
+                else
+                {
+                    Groups.Add(new Group { Name = GroupName, RunnersNames = new ObservableCollection<string>() });
+                }
+            });
+        }
+
+        public ICommand CreateNewGroup { get; private set; }
     }
 }
